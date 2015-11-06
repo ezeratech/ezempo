0dc9d9787b7bd3dfd49bf5670b0e4d41008a882c
Developer <dev@Pair2.local>
2015-11-06 11:12:48 +0800

    setup login and forget password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Letter Opener
+  config.action_mailer.delivery_method = :letter_opener
+  
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
